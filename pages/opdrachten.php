eeea0469d9f4e9e823a3443769a9d007f3c95690
IJuanTM <iwan2510@gmail.com>
2019-10-16 15:24:59 +0200

    Zoekbalk navbar verder van af

---
+++ pages/opdrachten.php
@@ -0,0 +1,49 @@
+<div class="row">
+
+    <div class="col-4"></div>
+
+    <div class="col-4">
+        <form method="post" action="">
+            <div class="form-row">
+
+                <div class="col">
+                    <div class="input-group">
+
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">Wat:</div>
+                        </div>
+
+                        <input type="text" class="form-control" id="trefwoorden" placeholder="Trefwoorden">
+
+                    </div>
+                </div>
+
+                <div class="col-5">
+                    <div class="input-group">
+
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">Waar:</div>
+                        </div>
+
+                        <select class="custom-select" id="locatie">
+                            <option selected>Kies Locatie...</option>
+                            <option value="1">Amsterdam</option>
+                            <option value="2">Den Haag</option>
+                            <option value="3">Rotterdam</option>
+                            <option value="4">Utrecht</option>
+                        </select>
+
+                    </div>
+                </div>
+
+                <div class="col-auto">
+                    <button type="submit" class="btn btn-secondary">Zoeken</button>
+                </div>
+
+            </div>
+        </form>
+    </div>
+
+    <div class="col-4"></div>
+
+</div>
