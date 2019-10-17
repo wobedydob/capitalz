34e5862a365adb55fcf1ab80c93a512d6361b064
IJuanTM <iwan2510@gmail.com>
2019-10-17 16:23:07 +0200

    content calc

--- pages/registreer_bedrijf.php
+++ pages/registreer_bedrijf.php
@@ -1,6 +1,9 @@
 <div class="row">
+
     <div class="col-3"></div>
+
     <div class="col-6">
+
         <form action="./script/register-script-bedrijf" method="post">
             <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
@@ -9,6 +12,9 @@
             </div>
             <button type="submit" class="btn btn-success btn-lg btn-block">Submit</button>
         </form>
+
     </div>
+
     <div class="col-3"></div>
+
 </div>
