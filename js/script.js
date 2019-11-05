c8a89e3512097450015151b556ff8b31a8111bbb
IJuanTM <iwan2510@gmail.com>
2019-11-05 14:29:20 +0100

    Footer collapse weg en terug naar scrollerinos

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
 particlesJS.load('particles-js', 'particles.json');
 
-$("#footer-toggle").click(function (e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-});
-
-$(".rotate").click(function () {
-    $(this).toggleClass("down");
-});
-
 const hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("is-active");
--- layout/footer.php
+++ layout/footer.php
@@ -1,42 +1,36 @@
 <footer class="footer">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-2 col-md-3 col-lg-4"></div>
-            <div class="footer-copyright col-8 col-md-6 col-lg-4">
+            <div class="footer-copyright col-12">
                 <div class="text-center">© 2019 Copyright:
                     <a href="../index.php"> CapitalZ</a>
                 </div>
             </div>
-            <div class="col-2 col-md-3 col-lg-4 footer-toggle">
-                <a class="rotate btn footer-icon" id="footer-toggle" data-toggle="collapse" data-target="#footer-collapse"><i class="fas fa-chevron-up"></i></a>
-            </div>
         </div>
-        <div class="collapse" id="footer-collapse">
-            <div class="row">
-                <div class="col-12 col-md-4 col-lg-4">
-                    <h5 class="footer-header">Welkom bij CapitalZ!</h5>
-                    <hr class="footer-row">
-                    <h6 class="footer-text">CapitalZ bied u als zzp'er een makkelijke en overzichtelijke site voor u om opdrachten te vinden en voor bedrijven een manier om makkelijk hun opdrachten te kunnen aanbieden!</h6>
-                </div>
-                <div class="col-0 col-md-1 col-lg-2"></div>
-                <div class="col-12 col-md-3 col-lg-3">
-                    <h5 class="footer-header">Links</h5>
-                    <hr class="footer-row">
-                    <ul class="list-unstyled footer-text">
-                        <li><a href="../index.php?content=contact">Contact</a></li>
-                        <li><a href="../index.php?content=over_ons">Over ons</a></li>
-                    </ul>
-                </div>
-                <div class="col-12 col-md-4 col-lg-3">
-                    <h5 class="footer-header">Bedrijf</h5>
-                    <hr class="footer-row">
-                    <ul class="list-unstyled footer-text">
-                        <li>MBO Utrecht</li>
-                        <li><a href="https://goo.gl/maps/inkn95VypsHBKuyy8" target="_blank">Daltonlaan 300</a></li>
-                        <li>3584 BK Utrecht</li>
-                        <li><a href="tel:0302815100" class="tel">030 - 28 15 100</a></li>
-                    </ul>
-                </div>
+        <div class="row">
+            <div class="col-12 col-md-4 col-lg-4">
+                <h5 class="footer-header">Welkom bij CapitalZ!</h5>
+                <hr class="footer-row">
+                <h6 class="footer-text">CapitalZ bied u als zzp'er een makkelijke en overzichtelijke site voor u om opdrachten te vinden en voor bedrijven een manier om makkelijk hun opdrachten te kunnen aanbieden!</h6>
+            </div>
+            <div class="col-0 col-md-1 col-lg-2"></div>
+            <div class="col-12 col-md-3 col-lg-3">
+                <h5 class="footer-header">Links</h5>
+                <hr class="footer-row">
+                <ul class="list-unstyled footer-text">
+                    <li><a href="../index.php?content=contact">Contact</a></li>
+                    <li><a href="../index.php?content=over_ons">Over ons</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-4 col-lg-3">
+                <h5 class="footer-header">Bedrijf</h5>
+                <hr class="footer-row">
+                <ul class="list-unstyled footer-text">
+                    <li>MBO Utrecht</li>
+                    <li><a href="https://goo.gl/maps/inkn95VypsHBKuyy8" target="_blank">Daltonlaan 300</a></li>
+                    <li>3584 BK Utrecht</li>
+                    <li><a href="tel:0302815100" class="tel">030 - 28 15 100</a></li>
+                </ul>
             </div>
         </div>
     </div>
