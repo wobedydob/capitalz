649a5122d6387b47f863d98fa0751e1d875912c6
IJuanTM <iwan2510@gmail.com>
2019-12-16 14:23:24 +0100

    Database fix + about page

--- capitalz_db.sql
+++ capitalz_db.sql
@@ -3,9 +3,9 @@
 -- Model: New Model    Version: 1.0
 -- MySQL Workbench Forward Engineering
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
+SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
+SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
 -- Schema mydb
@@ -13,74 +13,85 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema capitalz_db
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `capitalz_db` ;
+DROP SCHEMA IF EXISTS `capitalz_db`;
 
 -- -----------------------------------------------------
 -- Schema capitalz_db
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `capitalz_db` DEFAULT CHARACTER SET latin1 ;
-USE `capitalz_db` ;
+CREATE SCHEMA IF NOT EXISTS `capitalz_db` DEFAULT CHARACTER SET latin1;
+USE `capitalz_db`;
 
 -- -----------------------------------------------------
 -- Table `capitalz_db`.`user_role`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `capitalz_db`.`user_role` ;
+DROP TABLE IF EXISTS `capitalz_db`.`user_role`;
 
-CREATE TABLE IF NOT EXISTS `capitalz_db`.`user_role` (
-  `role_id` INT(2) NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`role_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = latin1;
+CREATE TABLE IF NOT EXISTS `capitalz_db`.`user_role`
+(
+    `role_id` INT(2)      NOT NULL AUTO_INCREMENT,
+    `title`   VARCHAR(50) NOT NULL,
+    PRIMARY KEY (`role_id`)
+)
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 4
+    DEFAULT CHARACTER SET = latin1;
 
+INSERT INTO user_role(role_id, title)
+VALUES (0, 'admin');
+INSERT INTO user_role(role_id, title)
+VALUES (1, 'company');
+INSERT INTO user_role(role_id, title)
+VALUES (2, 'selfemployed');
 
 -- -----------------------------------------------------
 -- Table `capitalz_db`.`user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `capitalz_db`.`user` ;
+DROP TABLE IF EXISTS `capitalz_db`.`user`;
 
-CREATE TABLE IF NOT EXISTS `capitalz_db`.`user`(
-                                                   `user_id`  INT(7)         NOT NULL AUTO_INCREMENT,
-                                                   `email`    VARBINARY(271) NOT NULL,
-                                                   `password` VARCHAR(255)   NOT NULL,
-                                                   `userrole` INT(2)         NOT NULL,
-                                                   PRIMARY KEY (`user_id`),
-                                                   INDEX `user.user_role - user_role.role_id_idx` (`userrole` ASC),
-                                                   CONSTRAINT `user.user_role - user_role.role_id`
-                                                       FOREIGN KEY (`userrole`)
-                                                           REFERENCES `capitalz_db`.`user_role` (`role_id`)
-                                                           ON DELETE NO ACTION
-                                                           ON UPDATE NO ACTION
+CREATE TABLE IF NOT EXISTS `capitalz_db`.`user`
+(
+    `user_id`  INT(7)         NOT NULL AUTO_INCREMENT,
+    `email`    VARBINARY(271) NOT NULL,
+    `password` VARCHAR(255)   NOT NULL,
+    `userrole` INT(2)         NOT NULL,
+    PRIMARY KEY (`user_id`),
+    INDEX `user.user_role - user_role.role_id_idx` (`userrole` ASC),
+    CONSTRAINT `user.user_role - user_role.role_id`
+        FOREIGN KEY (`userrole`)
+            REFERENCES `capitalz_db`.`user_role` (`role_id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
 )
-ENGINE = InnoDB
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = latin1;
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 4
+    DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
 -- Table `capitalz_db`.`user_info`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `capitalz_db`.`user_info` ;
+DROP TABLE IF EXISTS `capitalz_db`.`user_info`;
 
-CREATE TABLE IF NOT EXISTS `capitalz_db`.`user_info` (
-  `user_id` INT(7) NOT NULL,
-  `key` VARCHAR(45) NOT NULL,
-  `value` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`user_id`),
-  CONSTRAINT `user.user_id - user_info.user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `capitalz_db`.`user` (`user_id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+CREATE TABLE IF NOT EXISTS `capitalz_db`.`user_info`
+(
+    `user_id` INT(7)      NOT NULL,
+    `key`     VARCHAR(45) NOT NULL,
+    `value`   VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`user_id`),
+    CONSTRAINT `user.user_id - user_info.user_id`
+        FOREIGN KEY (`user_id`)
+            REFERENCES `capitalz_db`.`user` (`user_id`)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
 -- Table `capitalz_db`.`job`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `capitalz_db`.`job` ;
+DROP TABLE IF EXISTS `capitalz_db`.`job`;
 
 CREATE TABLE IF NOT EXISTS `capitalz_db`.`job`
 (
@@ -99,14 +110,14 @@ CREATE TABLE IF NOT EXISTS `capitalz_db`.`job`
             ON DELETE NO ACTION
             ON UPDATE NO ACTION
 )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
 -- Table `capitalz_db`.`review`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `capitalz_db`.`review` ;
+DROP TABLE IF EXISTS `capitalz_db`.`review`;
 
 CREATE TABLE IF NOT EXISTS `capitalz_db`.`review`
 (
@@ -119,10 +130,10 @@ CREATE TABLE IF NOT EXISTS `capitalz_db`.`review`
     INDEX `user_id - user_id` (`user_id` ASC),
     INDEX `review.user_role - user.user_role_idx` (`user_role` ASC)
 )
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = latin1;
+    ENGINE = MyISAM
+    DEFAULT CHARACTER SET = latin1;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+SET SQL_MODE = @OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
