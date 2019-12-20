63e7e3881863423fe1182e980d122078eb45de63
Robin vd Hoeven <322961.ionic@gmail.com>
2019-12-20 18:22:22 +0100

    Register page changes

--- page/RegisterPage.php
+++ page/RegisterPage.php
@@ -30,28 +30,34 @@ class RegisterPage
             $email = ApplicationController::sanitize($_POST['email']);
             $password = ApplicationController::sanitize($_POST['password']);
             $repeat_password = ApplicationController::sanitize($_POST['repeat_password']);
-            $bnummer = ApplicationController::sanitize($_POST['bnummer']);
-
-            if (!isset($email)) {
+            $btw_nummer = ApplicationController::sanitize($_POST['btw_nummer']);
+            if (isset($email)) {
                 $db = new Database();
-                $db->query("SELECT * from `user` WHERE `email` = ':email'");
+                $db->query("SELECT * from `user` WHERE `email` = :email");
                 $db->bind(':email', $email);
-
+//                var_dump('SELECT * from `user` WHERE `email` = :email');
+//                var_dump($email);
                 $db->execute();
-                var_dump($db->resultset());
 
 
-                if (mysqli_num_rows($result) > 0) {
-                    header("refresh:2; url=./index.php?content=register");
+                if ($db->rowCount() > 0) {
+                    header("refresh:2; url=/login");
                     die('<div class="alert alert-danger" role="alert">
                             Dit e-mail adres is al in gebruik
                          </div>');
                 } else {
                     $password = 'geheim';
                     $password_hash = password_hash($password, PASSWORD_BCRYPT);
-
                     $db = new Database();
-                    $db->query("INSERT INTO `user`(`user_id`, `email`, `password`, `userrole`) VALUES(NULL, $email, $password_hash, 2)");
+                    $db->query("INSERT INTO `user` (`user_id`, `email`, `password`, `userrole`) VALUES(NULL, :email, :password_hash, 2)");
+                    $db->bind(':email', $email);
+                    $db->bind(':password_hash', $password_hash);
+                    $db->execute();
+                    $db->bind(':user_id', $user_id = $db->lastInsertId());
+                    $db->execute();
+                    $db->query("INSERT INTO `user_info` (`user_id`, `key`, `value`) VALUES(:user_id, 'Btw-nummer', $btw_nummer)");
+                    $db->execute();
+                    var_dump($db);
                 }
             }
         }
