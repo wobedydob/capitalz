fe4562cdd38c39433769e104562a5ae78f58e796
wub <gitwub@gmail.com>
2020-01-14 21:25:44 +0100

    started work on createPage

--- page/RegisterPage.php
+++ page/RegisterPage.php
@@ -51,12 +51,6 @@ class RegisterPage
                         $db->bind(':password_hash', $password_hash);
                         $db->bind(':btw_nummer', $btw_nummer);
                         $db->execute();
-//                        $userId = $db->lastInsertId();
-//                        $db->query("INSERT INTO `user_info` (`user_id`, `key`, `value`) VALUES(:user_id, 'btw-nummer', :btw_nummer)");
-//                        $db->bind(':user_id', $userId);
-//                        $db->bind(':btw_nummer', $btw_nummer);
-//                        $db->execute();
-//                    var_dump($db);
                         echo '<div class="alert alert-success" role="alert">Je bent geregistreerd, je kan nu inloggen</div>';
                         header("refresh:1; url=../login");
                     }
@@ -75,20 +69,19 @@ class RegisterPage
             $password = ApplicationController::sanitize($_POST['password']);
             $repeat_password = ApplicationController::sanitize($_POST['repeat_password']);
             $kvk_nummer = ApplicationController::sanitize($_POST['kvk_nummer']);
-            if ($repeat_password == $password) {
-                if (isset($email)) {
-                    $db = new Database();
-                    $db->query("SELECT * from `user` WHERE `email` = :email");
-                    $db->bind(':email', $email);
+            if (isset($email)) {
+                $db = new Database();
+                $db->query("SELECT * from `user` WHERE `email` = :email");
+                $db->bind(':email', $email);
 //                var_dump('SELECT * from `user` WHERE `email` = :email');
 //                var_dump($email);
-                    $db->execute();
+                $db->execute();
 
+                if ($repeat_password == $password) {
                     if ($db->rowCount() > 0) {
-                        header("refresh:2; url=/create");
+                        header("refresh:2; url=/login");
                         die('<div class="alert alert-danger" role="alert">Dit e-mail adres is al in gebruik</div>');
                     } else {
-                        //$password = 'geheim';
                         $password_hash = password_hash($password, PASSWORD_BCRYPT);
                         $db = new Database();
                         $db->query("INSERT INTO `user` (`email`, `password`, `number`, `user_role`) VALUES(:email, :password_hash, :kvk_nummer, 1)");
@@ -96,12 +89,8 @@ class RegisterPage
                         $db->bind(':password_hash', $password_hash);
                         $db->bind(':kvk_nummer', $kvk_nummer);
                         $db->execute();
-//                        $userId = $db->lastInsertId();
-//                        $db->query("INSERT INTO `user_info` (`user_id`, `key`, `value`) VALUES(:user_id, 'kvk-nummer', $kvk_nummer)");
-//                        $db->bind(':user_id', $userId);
-//                        $db->execute();
-//                    var_dump($db);
                         echo '<div class="alert alert-success" role="alert">Je bent geregistreerd, je kan nu inloggen</div>';
+                        header("refresh:1; url=../login");
                     }
                 } else {
                     echo 'De wachtwoorden komen niet overeen';
