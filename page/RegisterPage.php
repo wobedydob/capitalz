aadb18c769831a1e40164c6a7075f26d7350aac9
IJuanTM <iwan2510@gmail.com>
2020-01-17 12:59:48 +0100

    Register notification fix

--- page/RegisterPage.php
+++ page/RegisterPage.php
@@ -8,6 +8,8 @@ class RegisterPage
     public $zzpForm;
     public $bedrijfForm;
 
+    public $formMessage;
+
     public function __construct($urlArr)
     {
         $this->urlArr = $urlArr;
@@ -25,12 +27,12 @@ class RegisterPage
 
     private function register_zzp()
     {
-        $this->zzpForm = ApplicationController::get_part_string('register/zzp', array('baseUrl' => $this->urlArr['baseUrl']));
         if (isset($_POST['reg_submit'])) {
             $email = ApplicationController::sanitize($_POST['email']);
             $password = ApplicationController::sanitize($_POST['password']);
             $repeat_password = ApplicationController::sanitize($_POST['repeat_password']);
             $btw_nummer = ApplicationController::sanitize($_POST['btw_nummer']);
+//            var_dump($_POST);
             if (isset($email)) {
                 $db = new Database();
                 $db->query("SELECT * from `user` WHERE `email` = :email");
@@ -41,8 +43,7 @@ class RegisterPage
 
                 if ($repeat_password == $password) {
                     if ($db->rowCount() > 0) {
-                        header("refresh:2; url=/login");
-                        die('<div class="alert alert-danger" role="alert">Dit e-mail adres is al in gebruik</div>');
+                        $this->formMessage = '<div class="alert alert-danger" role="alert">Dit e-mail adres is al in gebruik</div>';
                     } else {
                         $password_hash = password_hash($password, PASSWORD_BCRYPT);
                         $db = new Database();
@@ -51,19 +52,24 @@ class RegisterPage
                         $db->bind(':password_hash', $password_hash);
                         $db->bind(':btw_nummer', $btw_nummer);
                         $db->execute();
-                        echo '<div class="alert alert-success" role="alert">Je bent geregistreerd, je kan nu inloggen</div>';
-                        header("refresh:1; url=../login");
+                        $this->formMessage = '<div class="alert alert-success" role="alert">Je bent geregistreerd, je kan nu inloggen</div>';
+                        header("refresh:1; url=" . $this->urlArr['baseUrl'] . "login");
                     }
                 } else {
-                    echo 'De wachtwoorden komen niet overeen';
+                    $this->formMessage = '<div class="alert alert-danger" role="alert">De wachtwoorden komen niet overeen</div>';
                 }
             }
         }
+        $this->zzpForm = ApplicationController::get_part_string(
+            'register/zzp',
+            array(
+                'baseUrl' => $this->urlArr['baseUrl'],
+                'formMessage' => $this->formMessage)
+        );
     }
 
     private function register_bedrijf()
     {
-        $this->bedrijfForm = ApplicationController::get_part_string('register/bedrijf', array('baseUrl' => $this->urlArr['baseUrl']));
         if (isset($_POST['reg_submit'])) {
             $email = ApplicationController::sanitize($_POST['email']);
             $password = ApplicationController::sanitize($_POST['password']);
@@ -79,8 +85,7 @@ class RegisterPage
 
                 if ($repeat_password == $password) {
                     if ($db->rowCount() > 0) {
-                        header("refresh:2; url=/login");
-                        die('<div class="alert alert-danger" role="alert">Dit e-mail adres is al in gebruik</div>');
+                        $this->formMessage = '<div class="alert alert-danger" role="alert">Dit e-mail adres is al in gebruik</div>';
                     } else {
                         $password_hash = password_hash($password, PASSWORD_BCRYPT);
                         $db = new Database();
@@ -89,13 +94,19 @@ class RegisterPage
                         $db->bind(':password_hash', $password_hash);
                         $db->bind(':kvk_nummer', $kvk_nummer);
                         $db->execute();
-                        echo '<div class="alert alert-success" role="alert">Je bent geregistreerd, je kan nu inloggen</div>';
-                        header("refresh:1; url=../login");
+                        $this->formMessage = '<div class="alert alert-success" role="alert">U bent geregistreerd! Een admin controleerd uw aanmelding. U zult dan een bevestigings mail ontvangen.</div>';
+                        header("refresh:1; url=" . $this->urlArr['baseUrl'] . "login");
                     }
                 } else {
-                    echo 'De wachtwoorden komen niet overeen';
+                    $this->formMessage = '<div class="alert alert-danger" role="alert">De wachtwoorden komen niet overeen</div>';
                 }
             }
         }
+        $this->bedrijfForm = ApplicationController::get_part_string(
+            'register/bedrijf',
+            array(
+                'baseUrl' => $this->urlArr['baseUrl'],
+                'formMessage' => $this->formMessage)
+        );
     }
 }
