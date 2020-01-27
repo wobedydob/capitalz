45021c1ebc44d1d1f5e61a7936a276a77473f3b1
wub <gitwub@gmail.com>
2020-01-27 13:33:41 +0100

    yeet 2.0

--- page/ProfilePage.php
+++ page/ProfilePage.php
@@ -5,6 +5,8 @@ class ProfilePage
     public $zzpForm;
     public $bedrijfForm;
 
+    public $profileForm;
+
     private $urlArr;
     public $profileCard;
 
@@ -12,13 +14,35 @@ class ProfilePage
     {
         $this->urlArr = $urlArr;
 
+        $this->get_profile($urlArr['pageVars'][0]);
+
         // Selecting the first value in the pageVars array
         if ($urlArr['pageVars'][0] == 'zzp') {
-            $this->profile_zzp();
+            //$this->profile_zzp();
         } else if ($urlArr['pageVars'][0] == 'bedrijf') {
-            $this->profile_bedrijf();
+            //$this->profile_bedrijf();
         } else {
             header("Refresh: 2; url=" . $urlArr['baseUrl'] . "home");
+        }
+
+    }
+
+    private function get_profile($type)
+    {
+        if ($type == 'zzp') $table = 'profile_se';
+        else $table = 'profile_co';
+
+        $db = new Database();
+        $db->query('SELECT 
+	                            `user`.*, 
+                                `profile`.* 
+                            FROM `user` 
+                            LEFT JOIN ' . $table . ' AS `profile` USING(user_id)
+                            WHERE `user`.`user_id` = :userId;');
+        $db->bind(':userId', ApplicationController::sanitize($_SESSION['id']));
+
+        if ($userInfo = $db->single()) {
+            $this->profileForm = ApplicationController::get_part_string('profile/' . $type, array('baseUrl' => $this->urlArr['baseUrl'], 'userInfo' => $userInfo));
         }
 
     }
